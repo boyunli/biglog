0546e4da3c5b1464fdcd287040fcc3a65d9ffdc3
liling <liling@choudpar.com>
2018-01-15 19:02:44 +0800

    modify some bugs

--- src/main/java/com/jianke/biglog/hbase/AccessLogParser.java
+++ src/main/java/com/jianke/biglog/hbase/AccessLogParser.java
@@ -10,13 +10,13 @@ public class AccessLogParser {
             logRecord.setD(data[0]);
             logRecord.setE(data[1]);
             logRecord.setUid(data[2]);
-            logRecord.setU(data[2]);
-            logRecord.setR(data[2]);
-            logRecord.setEt(data[2]);
-            logRecord.setSi(data[2]);
-            logRecord.setS(data[2]);
-            logRecord.setF(data[2]);
-            logRecord.setHumanTime(data[3]);
+            logRecord.setU(data[3]);
+            logRecord.setR(data[4]);
+            logRecord.setEt(data[5]);
+            logRecord.setSi(data[6]);
+            logRecord.setS(data[7]);
+            logRecord.setF(data[8]);
+            logRecord.setHumanTime(data[9]);
             return logRecord;
         }
         return  null;
