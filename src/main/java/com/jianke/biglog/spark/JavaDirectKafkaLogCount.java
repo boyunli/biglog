07bf6fd46e21e5a8490b72c12e62d9f1ae61eab6
liling <liling@choudpar.com>
2018-01-17 13:34:36 +0800

    add spark direct streaming

---
+++ src/main/java/com/jianke/biglog/spark/JavaDirectKafkaLogCount.java
@@ -0,0 +1,56 @@
+package com.jianke.biglog.spark;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import scala.Tuple2;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.apache.spark.streaming.Durations;
+
+
+public final class JavaDirectKafkaLogCount {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception {
+
+        String brokers = "172.21.57.147:9092,172.21.57.148:9092,172.21.57.149:9092";
+        String topics = "biglog1,biglog";
+
+        // Create context with a 2 seconds batch interval
+        SparkConf sparkConf = new SparkConf().setAppName("JavaDirectKafkaLogCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
+
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("metadata.broker.list", brokers);
+
+        // Create direct kafka stream with brokers and topics
+        JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(
+                jssc,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.<String, String>Subscribe(topicsSet, kafkaParams));
+
+        // Get the lines, split them into words, count the words and print
+        JavaDStream<String> lines = messages.map(ConsumerRecord::value);
+        System.out.println("lines:" + lines);
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)));
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((i1, i2) -> i1 + i2);
+        wordCounts.print();
+
+        // Start the computation
+        jssc.start();
+        jssc.awaitTermination();
+    }
+}
